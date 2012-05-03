35b78fd4ec540dcb7e0b6b504f35986b910830b0
Adrian Ghizaru <adrian.ghizaru@gmail.com>
2012-05-03 15:33:07 -0400

    Added logging example to RootController

--- src/__project__/controllers/root.py
+++ src/__project__/controllers/root.py
@@ -1,5 +1,9 @@
 from web.core import Controller
 
+log = __import__('logging').getLogger(__name__)
+
+
 class RootController(Controller):
     def __default__(self, *args, **kwargs):
+        log.info('-- Request --')
         return '__project__'
