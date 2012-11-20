4d6d951d3c49464ed0be055d4665113a9427cdd2
Adrian Ghizaru <adrian.ghizaru@gmail.com>
2012-11-19 22:07:30 -0500

    Added pprint to admin_boot.py imports

--- src/__project__/commands/admin_boot.py
+++ src/__project__/commands/admin_boot.py
@@ -7,16 +7,23 @@ _ansi = {
     'mdl': ansi.green,
     'cls': ansi.blue,
     'obj': ansi.yellow,
+    'func': ansi.cyan,
     'r': ansi.reset
 }
 
 print ''
-print 'Available {mdl}modules{r}, {cls}classes{r} and {obj}objects{r}'.format(**_ansi)
-print '-' * 38
+print 'Available {mdl}modules{r}, {cls}classes{r}, {obj}objects{r} and {func}functions{r}'.format(**_ansi)
+print '-' * 49
 print 'import {mdl}web{r}'.format(**_ansi)
 import web
 print 'from web.core import {mdl}http{r}, {cls}Controller{r}, {obj}request{r}, {obj}response{r}'.format(**_ansi)
 from web.core import http, Controller, request, response
+#print 'from __project__ import {obj}settings{r}'.format(**_ansi)
+#from __project__ import settings
+#print 'from __project__.model import session as {obj}db{r}'.format(**_ansi)
+#from __project__.model import session as db
+print '{obj}app{r} = paste.fixture.TestApp'.format(**_ansi)
+print "          Make requests with app.get('<path>'), app.post('<path>', params='<params>'), etc."
 
 print 'from datetime import {cls}date{r}, {cls}time{r}, {cls}datetime{r}'.format(**_ansi)
 from datetime import date, time, datetime
@@ -24,12 +31,8 @@ from datetime import date, time, datetime
 #from dateutil.relativedelta import relativedelta
 #print 'from pytz import {cls}timezone{r}, {obj}utc{r}'.format(**_ansi)
 #from pytz import timezone, utc
-#print 'from __project__ import {obj}settings{r}'.format(**_ansi)
-#from __project__ import settings
-#print 'from __project__.model import session as {obj}db{r}'.format(**_ansi)
-#from __project__.model import session as db
-print '{obj}app{r} = paste.fixture.TestApp'.format(**_ansi)
-print "          Make requests with app.get('<path>'), app.post('<path>', params='<params>'), etc."
+print 'from pprint import {func}pprint{r}'.format(**_ansi)
+from pprint import pprint
 
 print ''
 print 'Admin shell'
